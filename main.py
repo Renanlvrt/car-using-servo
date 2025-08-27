2a45e6ee46d979ff57980045cf71edb8479840cd
renan <renan.lavirotte@gmail.com>
2025-08-27 12:14:37 +0900

    make the servos functionning

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 def on_forever():
+    motobit.enable(MotorPower.OFF)
     pass
 basic.forever(on_forever)
 
 def on_button_pressed_a():
-    pins.servo_write_pin(AnalogPin.P0, 90)
+    pins.servo_write_pin(AnalogPin.P15, 90)
+    basic.pause(100)
     pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    pins.servo_write_pin(AnalogPin.P0, 180)
+    pins.servo_write_pin(AnalogPin.P15, 180)
+    basic.pause(100)
     pass
 input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 basic.forever(function on_forever() {
+    motobit.enable(MotorPower.Off)
     
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    pins.servoWritePin(AnalogPin.P0, 90)
+    pins.servoWritePin(AnalogPin.P15, 90)
+    basic.pause(100)
     
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    pins.servoWritePin(AnalogPin.P0, 180)
+    pins.servoWritePin(AnalogPin.P15, 180)
+    basic.pause(100)
     
 })
